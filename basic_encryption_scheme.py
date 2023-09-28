e97b4e823dfb092564d6fb71caf04ab8d886092d
Shien Fu <ShienFu99@gmail.com>
2023-09-28 18:06:43 -0400

    Encrypt_symbol scheme can now be written to a separate file.

--- basic_encryption_scheme.py
+++ basic_encryption_scheme.py
@@ -6,6 +6,7 @@
 """
 
 #Built-in Imports
+import csv
 from itertools import chain
 from random import choice
 
@@ -15,7 +16,9 @@ def main():
     normal_symbols = list(chain(list(map(chr, range(97, 123))), list(map(chr, range(65, 90))), range(10)))
     #Normal symbols + special symbols -> ! " # $ % & ' ( ) * + , - . : ; < = > ? [ \ ] ^ _ { | }
     special_symbols = list(chain(list(map(chr, range(33, 47))), list(map(chr, range(58, 64))), list(map(chr, range(91, 96))), list(map(chr, range(123, 126))), normal_symbols))
-    encrypt_symbols = {}
+    special_symbols.remove("\'")
+    special_symbols.remove("\"")
+    encrypt_symbols = []
 
     #Prompts user for a number -> Represents the number of special_symbols to represent each normal_symbols
     while True:
@@ -33,16 +36,28 @@ def main():
         encrypt_symbol = generate_encrypt_symbol(num_of_symbols, special_symbols)
 
         #If the encrypt_symbol is a repeat of a previous one, generate a new one until it's a unique value
-        if encrypt_symbol in encrypt_symbols.values():
-            while encrypt_symbol in encrypt_symbols.values():
-                encrypt_symbol = generate_encrypt_symbol(num_of_symbols, special_symbols)
+        for d in encrypt_symbols:
+            if encrypt_symbol == d["encrypt_symbol"]:
+                while encrypt_symbol == d["encrypt_symbol"]:
+                    encrypt_symbol = generate_encrypt_symbol(num_of_symbols, special_symbols)
 
         #Creates a dictionary entry -> "normal_symbol": "encrypt_symbol"
-        encrypt_symbols[str(ch)] = encrypt_symbol
+        encrypt_symbols.append({"normal_symbol": str(ch), "encrypt_symbol": encrypt_symbol})
 
-    #Prints the dictionary of encrypt_symbols
-    for key, val in encrypt_symbols.items():
-        print(f"{key},{val}")
+    with open("encrypt_scheme.csv", "w") as new_file:
+        csv_writer = csv.DictWriter(new_file, fieldnames=["normal_symbol", "encrypt_symbol"])
+        #Prints the dictionary of encrypt_symbols
+
+        csv_writer.writeheader()
+
+        for d in encrypt_symbols:
+            csv_writer.writerow(d)
+
+    with open("encrypt_scheme.csv", "r") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+
+        for line in csv_reader:
+            print(line)
 
 
 #Write a docstring later -> Randomly pick N symbols from special_symbols and returns the string as an encrypt_symbol
