c2e47f6fb82afa1861f06c8e473566d05d07cbab
Shien Fu <ShienFu99@gmail.com>
2023-09-28 00:46:32 -0400

    First prototype of encryptor -> Prints a list of encrypt_symbols

---
+++ README.md
@@ -0,0 +1,18 @@
+## Purpose:
+
+Maps the latin alphabet + arabic numbers into a new set of "encrypt_symbols". Each encrypt_symbol is comprised of n special_symbols.
+
+special_symbols = [! " # $ % & ' ( ) * + , - . : ; < = > ? [ \ ] ^ _ { | } a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9]
+
+## Executing the program:
+
+Use python or python3 command depending on your installation:
+
+Command to execute program: python basic_encryption_scheme.py
+
+## Future implementation ideas:
+
+> Saves the encrypt_symbols in a file and takes the user's password. Using command-line arguments, allows the user to input their normal password and convert it to its encrypted form.
+> If the user wants, they can generate a new list of encrypt_symbols
+> *Do not save the user's password, convert it during runtime so they can copy/paste it*
+> *Backup the encrypt key somehow???*
---
+++ basic_encryption_scheme.py
@@ -0,0 +1,37 @@
+"""
+    Date (YYYY-MM-DD): 2023-09-28
+    Description: Maps a basic set of symbols (latin alphabet + arabic numbers) to a new list of "encrypt_symbols"
+    > The user inputs a number -> Represents how many "special_symbols" comprise an encrypt_symbol
+    > Example: if the user inputs 3, the letter "a" could become "!^&"
+
+"""
+
+#Built-in Imports
+from itertools import chain
+from random import choice
+
+
+def main():
+    #List of normal symbols -> the latin alphabet + numbers 0-9
+    normal_symbols = list(chain(list(map(chr, range(97, 123))), range(10)))
+    #Normal symbols + special symbols -> ! " # $ % & ' ( ) * + , - . : ; < = > ? [ \ ] ^ _ { | }
+    special_symbols = list(chain(list(map(chr, range(33, 47))), list(map(chr, range(58, 64))), list(map(chr, range(91, 96))), list(map(chr, range(123, 126))), normal_symbols))
+
+    #Prompts user for a number -> Represents the number of special_symbols to represent each normal_symbols
+    #Reprompts if integer value not inputted
+    while True:
+        try:
+            num_of_symbols = int(input("How many symbols per encrypt_symbol (ie: if 3, \"a\" -> $#@): "))
+            break
+        except ValueError:
+            pass
+
+    #Converts each normal symbol to n encrypt_symbols -> n represents num of symbols
+    for count in enumerate(normal_symbols):
+        for _ in range(num_of_symbols):
+            print(f"{choice(special_symbols)}", end="")
+        print()
+
+
+if __name__ == "__main__":
+    main()
