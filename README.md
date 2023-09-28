1d67baa8861cb74b86baf62af26fb195607c366e
Shien Fu <ShienFu99@gmail.com>
2023-09-28 14:24:31 -0400

    Save encrypt symbol scheme into a dictionary

--- README.md
+++ README.md
@@ -12,7 +12,16 @@ Command to execute program: python basic_encryption_scheme.py
 
 ## Future implementation ideas:
 
+> Prevent list from containing duplicates
 > Saves the encrypt_symbols in a file and takes the user's password. Using command-line arguments, allows the user to input their normal password and convert it to its encrypted form.
+
+Flags:
+> -h -> Prints helps
+> -n N -> Takes sets the number of special symbols per encrypt_symbol
+       -> Saves the encrypt_symbol scheme to a file
+> -c -> Prompts the user for their password and converts it
+     -> If encrypt_symbol scheme is not set yet, the user must set one first
+
 > If the user wants, they can generate a new list of encrypt_symbols
 > *Do not save the user's password, convert it during runtime so they can copy/paste it*
 > *Backup the encrypt key somehow???*
--- basic_encryption_scheme.py
+++ basic_encryption_scheme.py
@@ -3,7 +3,6 @@
     Description: Maps a basic set of symbols (latin alphabet + arabic numbers) to a new list of "encrypt_symbols"
     > The user inputs a number -> Represents how many "special_symbols" comprise an encrypt_symbol
     > Example: if the user inputs 3, the letter "a" could become "!^&"
-
 """
 
 #Built-in Imports
@@ -16,6 +15,8 @@ def main():
     normal_symbols = list(chain(list(map(chr, range(97, 123))), list(map(chr, range(65, 90))), range(10)))
     #Normal symbols + special symbols -> ! " # $ % & ' ( ) * + , - . : ; < = > ? [ \ ] ^ _ { | }
     special_symbols = list(chain(list(map(chr, range(33, 47))), list(map(chr, range(58, 64))), list(map(chr, range(91, 96))), list(map(chr, range(123, 126))), normal_symbols))
+    encrypt_symbol = ""
+    encrypt_symbols = {}
 
     #Prompts user for a number -> Represents the number of special_symbols to represent each normal_symbols
     #Reprompts if integer value not inputted
@@ -27,11 +28,14 @@ def main():
             pass
 
     #Converts each normal symbol to n encrypt_symbols -> n represents num of symbols
-    for count in enumerate(normal_symbols):
+    for count, ch in enumerate(normal_symbols):
         for _ in range(num_of_symbols):
-            print(f"{choice(special_symbols)}", end="")
-        print()
+            encrypt_symbol += str(choice(special_symbols))
+        encrypt_symbols[str(ch)] = encrypt_symbol
+        encrypt_symbol = ""
 
+    for key, val in encrypt_symbols.items():
+        print(f"{key},{val}")
 
 if __name__ == "__main__":
     main()
