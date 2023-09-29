3584e3c7653eef7f2e4a0c396a05f968dea5c7ee
Shien Fu <ShienFu99@gmail.com>
2023-09-28 22:46:44 -0400

    Added new flag -r to reuse previous encrypt_scheme + user can input a password and convert it based on the scheme

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 ## Purpose:
 
-Maps the latin alphabet (uppercase + lowercase) + arabic numbers into a new set of "encrypt_symbols". Each encrypt_symbol is comprised of n special_symbols.
+> First run = generate encryption scheme -> "Encryption scheme saved."
+> Subsequent runs = convert regular password using current encryption scheme
+> *Can also regenerate previous scheme -> Offer a warning to the user
 
-special_symbols = latin alphabet (uppercase + lowercase) + arabic numbers + [! # $ % & ( ) * + , - . : ; < = > ? [ \ ] ^ _ { | }]
+> How is the scheme saved -> In a regular file -> Stored locally
+> How are passwords stored?
+
+
+
+
+
+This program maps the latin alphabet (uppercase + lowercase) + the arabic numbers into a new list of "encrypt_symbols". Each encrypt_symbol is comprised of n "special_symbols", where n is between 2 and 7.
+
+special_symbols = latin alphabet (uppercase + lowercase) + arabic numbers + [! " # $ % & ' ( ) * + , - . : ; < = > ? [ \ ] ^ _ { | }]
+
+By typing in your regular password, you can get the encrypted form and only have to remember the original version.
+
 
 ## Executing the program:
 
@@ -12,13 +26,16 @@ Command to execute program: python basic_encryption_scheme.py
 
 ## Future implementation ideas:
 
-
+> Toggle visibility -> Display as ***** or chars
 > Take the user's password. Allows the user to input their normal password and convert it to its encrypted form.
 
 Flags:
 > -h -> Prints help
 > -n N -> Sets the number of special_symbols per encrypt_symbol
        -> Saves the encrypt_symbol scheme to a file
+> -r -> Reuse scheme?
+
+> Separate generating encryption scheme vs converting password
 
 > If the user wants, they can generate a new list of encrypt_symbols
 > *Do not save the user's password, convert it during runtime so they can copy/paste it*
--- basic_encryption_scheme.py
+++ basic_encryption_scheme.py
@@ -19,12 +19,15 @@ def main():
     #Normal symbols + special symbols -> ! " # $ % & ' ( ) * + , - . : ; < = > ? [ \ ] ^ _ { | }
     special_symbols = list(chain(list(map(chr, range(33, 47))), list(map(chr, range(58, 64))), list(map(chr, range(91, 96))), list(map(chr, range(123, 126))), normal_symbols))
 
-    #encrypt_symbols hold a list of dictionaries -> each dictionary contains the original symbol and the new combination of special_symbols to map it too
-    encrypt_symbols = []
+    #encrypt_scheme hold a list of dictionaries -> each dictionary contains the original symbol and the new combination of special_symbols to map it too
+    encrypt_scheme = []
+
+    generate = False
 
 
     parser = argparse.ArgumentParser(description="Maps a list of ordinary symbols to a list of \"encrypt_symbols\"")
     parser.add_argument("-n", default=3, help="number of special_symbols per encrypt_symbol - between 2 and 7 (inclusive)", type=int)
+    parser.add_argument("-r", help="reuse previous scheme", action='store_true')
 
     args = parser.parse_args()
 
@@ -34,36 +37,56 @@ def main():
     except ValueError:
         exit("Number must be between 2 and 7.")
 
-    #Converts each normal symbol into an encrypt_symbols
-    for count, ch in enumerate(normal_symbols):
+    try:
+        csv_file = open("encrypt_scheme.csv")
+        if file_empty(csv_file):
+            generate = True
+        csv_file.close()
+    except FileNotFoundError:
+        generate = True
 
-        #Generates an encrypt_symbol of size args.n
-        encrypt_symbol = generate_encrypt_symbol(args.n, special_symbols)
 
-        #If the encrypt_symbol is a repeat of a previous one, generate a new one until it's a unique value
-        for d in encrypt_symbols:
-            if encrypt_symbol == d["encrypt_symbol"]:
-                while encrypt_symbol == d["encrypt_symbol"]:
-                    encrypt_symbol = generate_encrypt_symbol(args.n, special_symbols)
+    #Opening the filename in "a" mode creates it if it doesn't already exist, allowing it to be checked for file contents
+    #If the file is empty or the user wants a new scheme, a new encrypt_scheme will be generated and saved to the file
+    with open("encrypt_scheme.csv", "a") as csv_file:
+        #if user inputs -r flag -> regenerate file
+        if args.r or generate == True:
+            print("User wants to generate a new scheme.")
+            clear_file(csv_file)
+            generate_encrypt_scheme(normal_symbols, special_symbols, args.n, encrypt_scheme, csv_file)
 
-        #Creates a dictionary entry -> "normal_symbol": "encrypt_symbol"
-        encrypt_symbols.append({"normal_symbol": str(ch), "encrypt_symbol": encrypt_symbol})
-
-    with open("encrypt_scheme.csv", "w") as new_file:
-        csv_writer = DictWriter(new_file, fieldnames=["normal_symbol", "encrypt_symbol"])
-        #Prints the dictionary of encrypt_symbols
-
-        csv_writer.writeheader()
-
-        for d in encrypt_symbols:
-            csv_writer.writerow(d)
-
-    #Opening the file will not throw an exception here as the previous block of code writes a file with the same name
+    #Opening the file in "r" mode will not throw an exception here as the previous block of code writes a file with the same name
     with open("encrypt_scheme.csv") as csv_file:
         csv_reader = DictReader(csv_file)
-
+        encrypt_scheme = []
         for line in csv_reader:
-            print(line)
+            encrypt_scheme.append({"normal_symbol": line['normal_symbol'], "encrypt_symbol": line['encrypt_symbol']})
+
+    user_password = input("Input your regular password: ")
+    encrypted_password = ""
+
+    for ch in user_password:
+        for d in encrypt_scheme:
+            if ch == d["normal_symbol"]:
+                encrypted_password += d["encrypt_symbol"]
+
+    print(f"Encrypted password: {encrypted_password}")
+
+
+#Working as intended
+def file_empty(file):
+    #If the first character of the file cannot be read, return True (file is empty), else False
+    if not file.read(1):
+        return True
+    return False
+
+
+#Working as intended
+def clear_file(file):
+    #Makes the file size 0 bytes
+    file.truncate(0)
+    #Repositions seek pointer to beginning of file
+    file.seek(0)
 
 
 #Write a docstring later -> Randomly pick N symbols from special_symbols and returns the string as an encrypt_symbol
@@ -72,6 +95,30 @@ def generate_encrypt_symbol(num_of_symbols, special_symbols):
     for _ in range(num_of_symbols):
         encrypt_symbol += str(choice(special_symbols))
     return encrypt_symbol
+
+
+def generate_encrypt_scheme(normal_symbols, special_symbols, num_of_symbols, encrypt_scheme, csv_file):
+    #Maps each normal symbol into an encrypt_symbols
+    for count, ch in enumerate(normal_symbols):
+
+        #Generates an encrypt_symbol of size num_of_symbols (args.n)
+        encrypt_symbol = generate_encrypt_symbol(num_of_symbols, special_symbols)
+
+        #If the encrypt_symbol is a repeat of a previous one, generate a new one until it's a unique value
+        for d in encrypt_scheme:
+            if encrypt_symbol == d["encrypt_symbol"]:
+                while encrypt_symbol == d["encrypt_symbol"]:
+                    encrypt_symbol = generate_encrypt_symbol(args.n, special_symbols)
+
+        #Creates a dictionary entry -> "normal_symbol": "encrypt_symbol"
+        encrypt_scheme.append({"normal_symbol": str(ch), "encrypt_symbol": encrypt_symbol})
+
+    csv_writer = DictWriter(csv_file, fieldnames=["normal_symbol", "encrypt_symbol"])
+
+    csv_writer.writeheader()
+
+    for d in encrypt_scheme:
+        csv_writer.writerow(d)
 
 
 if __name__ == "__main__":
